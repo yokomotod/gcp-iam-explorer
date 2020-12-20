c89914bc2a308a952bd2fadf07952e05b2225899
yokomotod <yokomotod@gmail.com>
2020-12-20 20:00:14 +0900

    fix material-ui ssr

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,13 +1,26 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { AppProps } from 'next/app';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { AppProps } from "next/app";
+import React from "react";
 import "../components/Splash.css";
 import "../styles/globals.css";
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <CssBaseline />
-    <Component {...pageProps} />
-  </>
-);
+const theme = createMuiTheme();
 
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 export default MyApp;
